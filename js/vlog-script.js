636746d9a3dd1ec54ee6d8da73b0d700d035cc9e
dharm1k987 <dharm1k987@gmail.com>
2017-02-17 08:28:01 -0500

    merged changes and fixed animation

---
+++ js/vlog-script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    
+    
+    
+    
+   $(".container-dharmik, .container-mirza, .container-vinit").mouseenter(function () {
+       var bg = $(this).css('backgroundColor');
+       
+       var rgb = bg.replace(/^rgba?\(|\s+|\)$/g,'').split(',');
+       var value = 'rgba('+rgb[0]+','+rgb[1]+','+rgb[2]+','+0.33+')';
+       console.log(value);
+       $(this).stop().animate({backgroundColor: value}, 10);
+       $(this).addClass('animate');
+
+
+       
+   });
+    
+    $(".container-dharmik, .container-mirza, .container-vinit").mouseleave(function () {
+       var bg = $(this).css('backgroundColor');
+       
+       var rgb = bg.replace(/^rgba?\(|\s+|\)$/g,'').split(',');
+       var value = 'rgba('+rgb[0]+','+rgb[1]+','+rgb[2]+','+0.09+')';
+       console.log(value);
+       $(this).stop().animate({backgroundColor: value}, 50);
+       $(this).removeClass('animate');
+
+
+       
+   });
+    
+    
+  
+});
