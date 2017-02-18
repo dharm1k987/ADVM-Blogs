bb74ca75b9bd6aa82bb5277d74451db384609d6d
dharm1k987 <dharm1k987@gmail.com>
2017-02-18 18:23:08 -0500

    updated vlog bg images

--- js/vlog-script.js
+++ js/vlog-script.js
@@ -4,12 +4,6 @@ $(document).ready(function () {
     
     
    $(".container-dharmik, .container-mirza, .container-vinit").mouseenter(function () {
-       var bg = $(this).css('backgroundColor');
-       
-       var rgb = bg.replace(/^rgba?\(|\s+|\)$/g,'').split(',');
-       var value = 'rgba('+rgb[0]+','+rgb[1]+','+rgb[2]+','+0.5+')';
-       console.log(value);
-       $(this).stop().animate({backgroundColor: value}, 10);
        $(this).addClass('animate');
 
 
@@ -17,12 +11,6 @@ $(document).ready(function () {
    });
     
     $(".container-dharmik, .container-mirza, .container-vinit").mouseleave(function () {
-       var bg = $(this).css('backgroundColor');
-       
-       var rgb = bg.replace(/^rgba?\(|\s+|\)$/g,'').split(',');
-       var value = 'rgba('+rgb[0]+','+rgb[1]+','+rgb[2]+','+0.3+')';
-       console.log(value);
-       $(this).stop().animate({backgroundColor: value}, 50);
        $(this).removeClass('animate');
 
 
