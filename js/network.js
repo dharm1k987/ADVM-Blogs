5a37198f0eb6af8a2651d5cc5c48928a097e644c
Vinit Soni <vinitsoni357@gmail.com>
2017-03-02 00:28:17 -0500

    Code Cleaning

--- js/network.js
+++ js/network.js
@@ -1,8 +1,6 @@
 
-$(document).ready(function () {
+window.onload = function () {
 
-    console.log(sessionStorage.getItem('correct-pwd'));
-    
     $(".container-dharmik, .container-mirza, .container-vinit, .container-Anonymous").click(function () {
         alert("in hover");
         $(this).addClass('animate');
@@ -11,21 +9,26 @@ $(document).ready(function () {
     $(".container-dharmik, .container-mirza, .container-vinit, .container-Anonymous").mouseleave(function () {
         $(this).removeClass('animate');
     });
-    console.log("passwed the func");
     var path = window.location.pathname;
-    $(".se-pre-con").fadeOut(3000);
+    $(".se-pre-con").fadeOut(2000);
     if (path === "/") {
-        initWebpage();
+        var push_btn = document.getElementById("upload-btn");
+        push_btn.onclick = function () {
+            window.location.replace("upload.html");
+        };
+        initMainWebpage();
+    } else if (path === "upload.html") {
+        var ret_btn = document.getElementById("ret-btn");
+        ret_btn.onclick = function () {
+            console.log("will now redirect");
+            document.location.pathname = "/";
+        }
     }
-    //initWebpage();
-    
+
     $("#btn-wts").click(function() {
         $("#show-text").show();
-    })
-
-
-
-});
+    });
+};
 
 
 
@@ -44,9 +47,9 @@ if (path === "/" && sessionStorage.getItem('correct-pwd') == "false") {
 
 $("#btn-pwd-redirect").click(function() {
    if ($("#pwd-redirect").val() == "gangsta") {
-       
+
        window.location.replace("/index.html");
-       initWebpage();
+       initMainWebpage();
        sessionStorage.setItem('correct-pwd',"true");
    }
     else {
@@ -76,67 +79,18 @@ var VERIFICATION_KEY = "gangsta";
 firebase.initializeApp(config);
 
 
-//postBlog('Vinit\'s Blog', 'Dharmik', 'date_goes_here', 'This is where the blog is');
-
-/*defaultDB.ref('/blogs').orderByChild('blogger').limitToFirst(2).once('value').then(function (blog) {
-    console.log("Running");
-    console.log(blog.val());
-});*/
-
-
-function dothis() {
-    getBlogAmount();
-};
-
-var increaseBlogAmount = function () {
-    blogAmountRef.once('value').then(function (blogAmount) {
-        var newBlogAmount = blogAmount.val() + 1;
-        console.log(newBlogAmount);
-        defaultDB.ref('/blogAmount').set(newBlogAmount);
-    })
-};
-
-var getBlogAmount = function () {
-    blogAmountRef.once('value').then(function (amount) {
-        document.getElementById('test').innerHTML = amount;
-        console.log(amount.val());
-        return amount.val();
-    });
-};
-
-function getBlog1(amount) {
-    var a = "blog" + 100;
-    blogsRef.set({
-        "blog100": {
-            blogText: "This is where the blog is",
-            blogger: "Vinit"
-        }
-    })
-    blogsRef.orderByKey().equalTo(a).once('value', function (blogList) {
-        document.getElementById('test').innerHTML = blogList.val()[a];
-        var m = blogList.val()[a];
-        //alert(m.blogger);
-        console.log(blogList.val()[a].blogger);
-    });
-
-};
-
-
-
-
-// Interface:
-// pushBlog();
-// getBlog(var  );
-
 ////////////////////////////////////////////////////////////////////////////////////
+// Index.html Module:
+
 var isLoggedIn = false;
 
-
-function initWebpage() {
+var initMainWebpage = function () {
     var user = firebase.auth().currentUser;
     if (user) {
         alert("There is a user logged in");
         alert(user.email);
+        isLoggedIn = true;
+
     } else {
         //Uncomment when implemented logging in.
         //loginProcess();
@@ -146,17 +100,101 @@ function initWebpage() {
         var mostRecentBlogID = "blog" + blogAmount;
         for (var i = 0; i < 5; i++) {
             getBlog("blog" + (blogAmount - i));
-            //alert(blogAmount - i);
         }
     });
-}
+};
+
+// Check for null objects. testing Required!
+var getBlog = function (blogID) {
+    //alert("ss");
+    blogsRef.orderByKey().equalTo(blogID.valueOf()).once('value', function (blogList) {
+        var blogTitle = blogList.child(blogID.valueOf()).val().blogTitle;
+        var blogDate = blogList.child(blogID.valueOf()).val().blogDate;
+        var blogAuthor = blogList.child(blogID.valueOf()).val().blogAuthor;
+        var blogNumber = "blog #" + blogID.substring(4);
+        var blogString = blogList.child(blogID.valueOf()).val().blogString;
+        //printComments(blogID, );
+        printBlog(blogID, blogTitle, blogDate, blogAuthor, blogString);
+    });
+};
+
+var printBlog = function (blogID, blogTitle, blogDate, blogAuthor, blogString) {
+    var body = document.body;
+
+    var firstname = getAuthorFirstname(blogAuthor);
+    var blogObjEl = document.createElement("div");
+    blogObjEl.className = "container container-" + firstname;
+
+    var randomDiv = document.createElement("div");
+    randomDiv.className = "clearfix";
+    blogObjEl.appendChild(randomDiv);
+
+    var blogTitleEl = document.createElement("h1");
+    blogTitleEl.appendChild(document.createTextNode(blogTitle));
+
+    var blogDateEl = document.createElement("h1");
+    blogDateEl.className = "date";
+    blogDateEl.appendChild(document.createTextNode(blogDate));
+
+    var authorImgFilename = firstname;
+    var authorText1 = '<img style=\"vertical-align:middle;border-radius:50%;filter:grayscale(50%)\" src=\"css/img/' + authorImgFilename + '.jpg\" alt=\"image\">';
+    var authorText2 = '<span style=\"margin-left:1%\">' + blogAuthor + '</span>';
+    var authorDiv = document.createElement("div");
+    authorDiv.innerHTML = authorText1 + authorText2;
+
+
+    var blogTextElDiv = document.createElement("div");
+    var blogTextElPara = document.createElement("p");
+    blogTextElDiv.className = "row text-justify col-md-12";
+    blogTextElPara.className = "message";
+    blogTextElPara.appendChild(document.createTextNode(blogString));
+    blogTextElDiv.appendChild(blogTextElPara);
+
+    //add comment section
+    var blogCommentTextInput = document.createElement('textarea');
+    var commentTitle = document.createElement('div');
+
+    commentTitle.innerHTML = "<strong>Comments:</strong>";
+    blogCommentTextInput.id = "commentText" + blogID;
+    blogCommentTextInput.placeholder = "Comment something...";
+    blogCommentTextInput.className = "reply-input";
+    blogCommentTextInput.cols = "100";
+    blogCommentTextInput.rows = "3";
+    blogCommentTextInput.maxLength = 200;
+
+    var blogCommentBtn = document.createElement("input");
+    blogCommentBtn.type = "button";
+    blogCommentBtn.value = "Post";
+    blogCommentBtn.className = "post-btn";
+
+    var commentDivision = document.createElement("div");
+    commentDivision.className = "comment-style";
+    commentDivision.id = "commentDivision" + blogID;
+
+    blogCommentBtn.onclick = loginProcess();
+
+    blogObjEl.appendChild(blogTitleEl);
+    blogObjEl.appendChild(blogDateEl);
+    blogObjEl.appendChild(authorDiv);
+    blogObjEl.appendChild(blogTextElDiv);
+    blogObjEl.appendChild(commentTitle);
+    blogObjEl.appendChild(commentDivision);
+    blogObjEl.appendChild(blogCommentTextInput);
+    blogObjEl.appendChild(blogCommentBtn);
+    getComments(blogID);
+    body.appendChild(blogObjEl);
+};
 
 var loginProcess = function () {
     $("#login-modal").modal('show');
 
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    var loginBtn = document.getElementById("login-btn");
+
     $(".loginmodal-submit").click(function () {
         firebase.auth().signInWithEmailAndPassword(username.value, password.value).then(function (resolved) {
-            initWebpage();
+            initMainWebpage();
         }, function (error) {
             var errorCode = error.code;
             var errorMsg = error.message;
@@ -167,8 +205,15 @@ var loginProcess = function () {
     });
 };
 
+////////////////////////////////////////////////////////////////////////////////////
 
-function getDate() {
+
+
+
+
+
+
+function getCurrentTime() {
 
     var dt = new Date();
     var hours = dt.getHours();
@@ -185,19 +230,60 @@ function getDate() {
 
 }
 
-function getTime() {
+function getCurrentDate() {
 
     var d = new Date();
     var day = d.getDate();
     var month = d.getMonth();
+    var monthString = "";
     var year = d.getFullYear();
+
+    switch month {
+        case 0:
+            monthString = "January";
+            break;
+        case 1:
+            monthString = "February";
+            break;
+        case 2:
+            monthString = "March";
+            break;
+        case 3:
+            monthString = "April";
+            break;
+        case 4:
+            monthString = "May";
+            break;
+        case 5:
+            monthString = "June";
+            break;
+        case 6:
+            monthString = "July";
+            break;
+        case 7:
+            monthString = "August";
+            break;
+        case 8:
+            monthString = "September";
+            break;
+        case 9:
+            monthString = "October";
+            break;
+        case 10:
+            monthString = "November";
+            break;
+        case 11:
+            monthString = "December";
+            break;
+    }
+
 
     if (month < 10) {
         month = "0" + (month + 1);
     } else {
         month = month + 1;
     }
-    return (String(month) + "/" + String(day) + "/" + String(year));
+    return (monthString + " " + String(day) + ", " + String(year));
 
 }
 
@@ -207,8 +293,6 @@ var getCurrentBlogAmount = -1;
 var updateBlogAmount = function () {};
 
 var postBlog = function () {
-    //var updatedBlogAmount = updateBlogAmount();
-    //var str = "blog"+updatedBlogAmount;
 
     blogAmountRef.once('value', function (blogAmount) {
         var updatedAmount = blogAmount.val() + 1;
@@ -218,28 +302,11 @@ var postBlog = function () {
 
         var blogID = "blog" + updatedAmount;
         var blogTitle = document.getElementById('titleInput').value;
-        /*var blogDate = document.getElementById('dayInput').value + "-" +
-            document.getElementById('monthInput').value + "-" +
-            document.getElementById('yearInput').value;*/
-
-        /*var dt = new Date();
-        var hours = dt.getHours();
-        var minute = dt.getMinutes();
-        var period = "AM";
-        if (hours > 12) {
-            hours = hours-12;
-            period = "PM";
-        }*/
 
         var blogDate = document.getElementById('dayInput').value + "-" +
             document.getElementById('monthInput').value + "-" +
-            document.getElementById('yearInput').value + " - " + getDate();
+            document.getElementById('yearInput').value + " - " + getCurrentTime();
 
-        /* var blogID = "blog" + updatedAmount;
-         var blogTitle = document.getElementById('titleInput').value;
-         var blogDate = document.getElementById('dayInput').value + "-" +
-             document.getElementById('monthInput').value + "-" +
-             document.getElementById('yearInput').value;*/
         var authorArr = document.getElementsByName('author');
         var blogAuthor = "";
         for (var i = 0; i < authorArr.length; i++) {
@@ -326,7 +393,7 @@ var getComments = function (blogID) {
                     divider.className = "divider";
                     var dateTimeComment = document.createElement('div');
                     dateTimeComment.className = "dtComment";
-                    dateTimeComment.innerHTML = getDate() + " - " + getTime();
+                    // NEED TO SETUP THE COMMENT'S DATE HERE
 
 
 
@@ -335,7 +402,7 @@ var getComments = function (blogID) {
                     commentsSection.appendChild(dateTimeComment);
                     commentsSection.appendChild(divider);
                     commentsSection.appendChild(document.createElement("br"));
-                    //commentsSection.appendChild(br);
+
 
                 });
             }
@@ -345,141 +412,9 @@ var getComments = function (blogID) {
 };
 
 
-var printBlog = function (blogID, blogTitle, blogDate, blogAuthor, blogString) {
-    var body = document.body;
-    var firstname = getAuthorFirstname(blogAuthor);
-    var blogObjEl = document.createElement("div");
-    blogObjEl.className = "container container-" + firstname;
-
-    var randomDiv = document.createElement("div");
-    randomDiv.className = "clearfix";
-    blogObjEl.appendChild(randomDiv);
-
-    var blogTitleEl = document.createElement("h1");
-    blogTitleEl.appendChild(document.createTextNode(blogTitle));
-
-    var blogDateEl = document.createElement("h1");
-    blogDateEl.className = "date";
-    blogDateEl.appendChild(document.createTextNode(blogDate));
-
-    var authorImgFilename = firstname;
-    var authorText1 = '<img style=\"vertical-align:middle;border-radius:50%;filter:grayscale(50%)\" src=\"css/img/' + authorImgFilename + '.jpg\" alt=\"image\">';
-    var authorText2 = '<span style=\"margin-left:1%\">' + blogAuthor + '</span>';
-    var authorDiv = document.createElement("div");
-    authorDiv.innerHTML = authorText1 + authorText2;
-
-    /*var randomDiv2 = document.createElement("div");
-    randomDiv2.className = "hr";*/
-
-
-    var blogTextElDiv = document.createElement("div");
-    var blogTextElPara = document.createElement("p");
-    blogTextElDiv.className = "row text-justify col-md-12";
-    blogTextElPara.className = "message";
-    blogTextElPara.appendChild(document.createTextNode(blogString));
-    blogTextElDiv.appendChild(blogTextElPara);
-
-    //add comment section
-    var blogCommentTextInput = document.createElement('textarea');
-    var commentTitle = document.createElement('div');
-
-    commentTitle.innerHTML = "<strong>Comments:</strong>";
-    blogCommentTextInput.id = "commentText" + blogID;
-    blogCommentTextInput.placeholder = "Comment something...";
-    blogCommentTextInput.className = "reply-input";
-    blogCommentTextInput.cols = "100";
-    blogCommentTextInput.rows = "3";
-    blogCommentTextInput.maxLength = 200;
-
-    var blogCommentBtn = document.createElement("input");
-    blogCommentBtn.type = "button";
-    blogCommentBtn.value = "Post";
-    blogCommentBtn.className = "post-btn";
-
-    var commentDivision = document.createElement("div");
-    commentDivision.className = "comment-style";
-    commentDivision.id = "commentDivision" + blogID;
 
 
 
-
-
-    blogCommentBtn.onclick = function () {
-        console.log(isLoggedIn);
-        if (isLoggedIn === false) {
-            $("#login-modal").modal('show');
-
-            $(".loginmodal-submit").click(function () {
-                firebase.auth().signInWithEmailAndPassword(username.value, password.value).catch(function (error) {
-                    var errorCode = error.code;
-                    var errorMsg = error.message;
-                    alert(errorMsg);
-                    alert("logged in?");
-                });
-                //loggedIn = true;
-                //alert("Logged in, you may now comment");
-                $("#login-modal").modal('hide');
-            });
-        } else {
-            commentDivision.innerHTML = "";
-            postComment(blogID, blogCommentTextInput.value)
-            blogCommentTextInput.value = "";
-        }
-
-    };
-
-    var username = document.getElementById("username");
-    var password = document.getElementById("password");
-
-    var loginBtn = document.getElementById("login-btn");
-
-
-
-
-    /*var randomDiv3 = document.createElement("div");
-    randomDiv3.className = "hr";*/
-
-
-
-
-    blogObjEl.appendChild(blogTitleEl);
-    blogObjEl.appendChild(blogDateEl);
-    blogObjEl.appendChild(authorDiv);
-    //blogObjEl.appendChild(randomDiv2);
-    blogObjEl.appendChild(blogTextElDiv);
-    //blogObjEl.appendChild(randomDiv3);
-
-    blogObjEl.appendChild(commentTitle);
-
-    blogObjEl.appendChild(commentDivision);
-
-
-    //blogObjEl.appendChild(randomDiv2);
-    blogObjEl.appendChild(blogCommentTextInput);
-
-    blogObjEl.appendChild(blogCommentBtn);
-    getComments(blogID);
-
-    body.appendChild(blogObjEl);
-};
-
-// Check for null objects. testing Required!
-var getBlog = function (blogID) {
-    //alert("ss");
-    blogsRef.orderByKey().equalTo(blogID.valueOf()).once('value', function (blogList) {
-        var blogTitle = blogList.child(blogID.valueOf()).val().blogTitle;
-        var blogDate = blogList.child(blogID.valueOf()).val().blogDate;
-        var blogAuthor = blogList.child(blogID.valueOf()).val().blogAuthor;
-        var blogNumber = "blog #" + blogID.substring(4);
-        var blogString = blogList.child(blogID.valueOf()).val().blogString;
-        //printComments(blogID, );
-        printBlog(blogID, blogTitle, blogDate, blogAuthor, blogString);
-
-
-
-
-    });
-};
 
 
 
@@ -507,7 +442,6 @@ var makeToast = function (outputString, outputStatus) {
 var reassureBeforePost = function () {
     var password = document.getElementById("passwordField").value;
     if (password === VERIFICATION_KEY) {
-        //alert("Item Deletion not implemented yet. Please revise before posting. Then, click the Green \"Confirmation Button\"");
         makeToast("VERIFIED", 1);
         document.getElementById('confirmedPushBtn').style.visibility = "visible";
         document.getElementById('blogPushBtn').style.visibility = "hidden";
@@ -515,19 +449,9 @@ var reassureBeforePost = function () {
         makeToast("Wrong password", 0);
     }
 }
-window.onload = function () {
-    var push_btn = document.getElementsByClassName("push-btn")[0];
-    push_btn.onclick = function () {
-        window.location.replace("/upload.html");
-    }
-};
 
 
-var ret_btn = document.getElementById("ret-btn");
-ret_btn.onclick = function () {
-    console.log("will now redirect");
-    document.location.pathname = "/";
-}
+
 
 var btn_today = document.getElementById("btnToday");
 btn_today.onclick = function () {
